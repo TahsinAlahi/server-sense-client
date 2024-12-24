ea022ef56ff88141371596ada1da3dabd8b140df
Tahsin <74871776+TahsinAlahi@users.noreply.github.com>
2024-12-24 19:44:17 +0000

    created: useAxiosPublic

---
+++ src/hooks/useAxiosPublic.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const axiosPublic = axios.create({
+ baseURL: 'https://b10a11-server-side-tahsin-alahi.vercel.app',
+ withCredentials: true,
+ timeout: 10000, // 10 seconds
+
+});
+
+function useAxiosPublic() {
+ return axiosPublic;
+}
+export default useAxiosPublic;
